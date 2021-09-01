e1ea2ccfa197ddbc3251165c3e89b5fa8ee99f35
MariviEscudero <mariviesmo@gmail.com>
2021-09-01 09:25:21 +0200

    add console log random number

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-"use strict";
+'use strict';
+
+const randomNumber = getRandomNumber();
+function getRandomNumber(max) {
+    max = 100;
+    return Math.ceil(Math.random() * max);
+   }
+
+console.log(`el numero es ${randomNumber}`);
