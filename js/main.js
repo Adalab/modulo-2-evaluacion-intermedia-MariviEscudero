5a8909ef03655ea78f262fc9fb9d0ebec296bc97
MariviEscudero <mariviesmo@gmail.com>
2021-09-08 23:01:58 +0200

    Extract the feedback painter to a function

--- js/main.js
+++ js/main.js
@@ -15,9 +15,9 @@ console.log(`Numero aleatorio = ${randomNumber}`);
 
 const attemptButton = document.querySelector('.js_attemptbutton');
 const userNumber = document.querySelector('.js_usernumber');
+console.log(userNumber.value);
 
-function handlePlayClues() {
-  console.log('Numero del usuario = ' + userNumber.value);
+function paintClues() {
   const parseUserNumber = parseInt(userNumber.value);
   if (parseUserNumber < 1) {
     document.getElementById('clue').value =
@@ -29,6 +29,16 @@ function handlePlayClues() {
     document.getElementById('clue').value = 'Demasiado alto';
   } else if (parseUserNumber < randomNumber) {
     document.getElementById('clue').value = 'Demasiado bajo';
+  } else if (parseUserNumber === '') {
+    document.getElementById('clue').value =
+      'El número debe estar entre 1 y 100';
   } else document.getElementById('clue').value = 'Has ganado campeona!!!';
 }
+
+function handlePlayClues() {
+  console.log('Numero del usuario = ' + userNumber.value);
+
+  paintClues();
+}
+
 attemptButton.addEventListener('click', handlePlayClues);
