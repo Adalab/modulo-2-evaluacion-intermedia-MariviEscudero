93f73c29feeb073a4186cd86336fcf422f5e19e4
MariviEscudero <mariviesmo@gmail.com>
2021-09-12 09:26:29 +0200

    Add counter function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const randomNumber = getRandomNumber();
+let counter = 0;
 
 //nuemro aleatorio
 
@@ -19,7 +20,7 @@ console.log(userNumber.value);
 
 //funcion que pinta las pistas
 
-function writeValue(message){
+function writeValue(message) {
   document.getElementById('clue').value = message;
 }
 
@@ -27,7 +28,7 @@ function paintClues() {
   const parseUserNumber = parseInt(userNumber.value);
   if (isNaN(parseUserNumber)) {
     writeValue('Tienes que escribir un número entre 1 y 100');
-  }else if(parseUserNumber < 1 || parseUserNumber > 100 ){
+  } else if (parseUserNumber < 1 || parseUserNumber > 100) {
     writeValue('El número debe estar entre 1 y 100');
   } else if (parseUserNumber > randomNumber) {
     writeValue('Demasiado alto');
@@ -35,13 +36,19 @@ function paintClues() {
     writeValue('Demasiado bajo');
   } else writeValue('Has ganado campeona!!!');
 }
+//contador
 
+function runCounter(){
+  counter++
+  document.getElementById('attempts').value = `Número de intentos: ${counter}`
+}
+  
 //listener click sobre el boton
 
 function handlePlayClues() {
   const userValue = userNumber.value;
   console.log(`Numero del usuario = ${userValue}`);
   paintClues();
+  runCounter();
 }
-
 attemptButton.addEventListener('click', handlePlayClues);
